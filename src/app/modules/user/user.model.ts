3274abb802f12a607892b741e89f9bb3a8c933a6
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-06-28 22:46:16 +0600

    [fet](password): hash password before sending to databage

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
-
+import bcrypt from 'bcrypt'
+import config from "../../config";
 
 const userSchema = new Schema<TUser>({
     name:{
@@ -10,6 +11,7 @@ const userSchema = new Schema<TUser>({
     password:{
         type:String,
         required:true,
+        select:0
 
     },
     email:{
@@ -41,5 +43,18 @@ const userSchema = new Schema<TUser>({
 }
 );
 
+userSchema.pre('save', async function(next){
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this;
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_rounds)
+    );
+    next();
+});
 
+userSchema.post('save', function(doc,next){
+    doc.password = '';
+    next();
+})
 export const User = model<TUser>('User', userSchema);
